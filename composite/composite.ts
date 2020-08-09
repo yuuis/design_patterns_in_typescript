d227f38d17bd053c86f481f9fb346fd249ba05e7
yu ishiguro <yu_ishiguro@fringe81.com>
2020-08-09 16:49:57 +0900

    5. rikai

--- composite/composite.ts
+++ composite/composite.ts
@@ -1,45 +1,38 @@
 namespace CompositePattern {
     export interface Component {
-        operation(): void;
+        print(): void;
     }
 
-    export class Composite implements Component {
-
+    // Directoryクラスは、Componentクラスのリストを持つ。
+    export class Directory implements Component {
         private list: Component[];
-        private s: String;
-
+        private readonly name: String;
         constructor(s: String) {
             this.list = [];
-            this.s = s;
+            this.name = s;
         }
 
-        public operation(): void {
-            console.log("`operation of `", this.s)
-            for (var i = 0; i < this.list.length; i += 1) {
-                this.list[i].operation();
-            }
+        // Directoryクラスのprintメソッドは、自身の配下を全て表示する
+        public print = (): void => {
+            console.log(`directory: ${this.name}`)
+            this.list.forEach(item => item.print())
         }
 
-        public add(c: Component): void {
-            this.list.push(c);
-        }
-
-        public remove(i: number): void {
-            if (this.list.length <= i) {
-                throw new Error("index out of bound!");
-            }
+        public add = (c: Component): number => this.list.push(c);
+        public remove = (i: number): void => {
+            if (this.list.length <= i) throw new Error("index out of bound!");
             this.list.splice(i, 1);
         }
     }
 
+    // Leafクラス
     export class Leaf implements Component {
-        private s: String;
+        private readonly name: String;
         constructor(s: String) {
-            this.s = s;
+            this.name = s;
         }
-        public operation(): void {
-            console.log("`operation` of Leaf", this.s, " is called.");
-        }
+
+        // Leafクラスのprintは自身を表示して終了。
+        public print = (): void => console.log(`leaf: ${this.name}`);
     }
 }
-
--- composite/demo.ts
+++ composite/demo.ts
@@ -1,25 +1,34 @@
 /// <reference path="composite.ts" />
 namespace CompositePattern {
-	export namespace Demo {
-		export function show() : void {
-		    var leaf1 = new CompositePattern.Leaf("1"),
-				leaf2 = new CompositePattern.Leaf("2"),
-				leaf3 = new CompositePattern.Leaf("3"),
+    export namespace Demo {
+        export function show() : void {
+            const leaf1 = new Leaf("Leaf1"),
+                leaf2 = new Leaf("Leaf2"),
+                leaf3 = new Leaf("Leaf3"),
 
-			composite1 = new CompositePattern.Composite("Comp1"),
-			composite2 = new CompositePattern.Composite("Comp2");
+                directory1 = new Directory("Dir1"),
+                directory2 = new Directory("Dir2");
 
-			composite1.add(leaf1);
-			composite1.add(leaf2);
-			composite1.add(leaf3);
+            directory1.add(leaf1);
+            directory1.add(leaf2);
+            directory1.add(leaf3);
 
-			composite1.remove(2);
+            directory1.remove(2);
+            directory1.print();
+            // dir1
+            // leaf1
+            // leaf2
 
-			composite2.add(leaf1);
-			composite2.add(leaf3);
-
-			composite1.operation();
-			composite2.operation();
-		}
-	}
+            directory2.add(directory1);
+            directory2.add(leaf3);
+            directory2.print();
+            // dir2
+            // dir1
+            // leaf1
+            // leaf2
+            // leaf3
+        }
+    }
 }
+
+CompositePattern.Demo.show();
