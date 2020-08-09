85db41190e3dbb0f0dbcc98a861029bd879f2ea5
yu ishiguro <yu_ishiguro@fringe81.com>
2020-08-09 19:22:57 +0900

    1. re rikai

--- factory_method/factoryMethod.ts
+++ factory_method/factoryMethod.ts
@@ -22,37 +22,31 @@ namespace FactoryMethodPattern {
     }
 
     // ファクトリのベースクラス。
-    // Factoryでどんなことを行うべきかメソッドとして定義する。
-    // 本来はメソッドで何をするべきか定義しておくけど、TSではできなかった。
-    export interface ProductFactory {
-        create(owner: string): Product;
-        // createは、本当はこう書きたい。
-        // create(owner: string): Product {
-        //     const product = createProduct(owner);
-        //     registerProduct(product);
-        //     return product;
-        // };
-        createProduct(owner: string): Product; // 本当はprotectedにしたい
-        registerProduct(product: Product): void; // 本当はprotectedにしたい
+    // Factoryでどんなことを行うべきかメソッドとして定義する。(= Template Method Pattern)
+    export class ProductFactoryBase {
+        create(owner: string): Product {
+            const product = this.createProduct(owner);
+            this.registerProduct(product);
+            return product;
+        };
+
+        // 以下のメソッドは、実装しないと使えないようにする。
+        createProduct = (owner: string): Product => {
+            throw new Error('Not Implemented.');
+        };
+        registerProduct = (product: Product): void => {
+            throw new Error('Not Implemented.');
+        };
     }
 
     // クラスごとのFactory。
-    export class CarFactoryImpl implements ProductFactory {
-        create = (owner: string): Product => {
-            const car = this.createProduct(owner);
-            this.registerProduct(car);
-            return car;
-        }
+    export class CarFactoryImpl extends ProductFactoryBase {
+        // createメソッドは親クラスのものをそのまま使うのでoverwriteしない。
         createProduct = (owner: string): Product => new Car(owner);
         registerProduct = (product: FactoryMethodPattern.Product) => console.log(`registered: ${JSON.stringify(product)}`);
     }
 
-    export class BicycleFactoryImpl implements ProductFactory {
-        create = (owner: string): Product => {
-            const car = this.createProduct(owner);
-            this.registerProduct(car);
-            return car;
-        }
+    export class BicycleFactoryImpl extends ProductFactoryBase {
         createProduct = (owner: string): Product => new Bicycle(owner);
         registerProduct = (product: FactoryMethodPattern.Product) => console.log(`registered: ${JSON.stringify(product)}`);
     }
